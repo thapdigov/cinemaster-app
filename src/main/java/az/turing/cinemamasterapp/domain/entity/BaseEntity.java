f4b62eacf835baa0aece2d9e5b5ccf9e0ab2068a
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-02-15 23:59:08 +0400

    UserEntity created

---
+++ src/main/java/az/turing/cinemamasterapp/domain/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package az.turing.cinemamasterapp.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ent_seq")
+    @SequenceGenerator(name = "ent_seq", sequenceName = "sequence_name", allocationSize = 1)
+    private Long id;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
