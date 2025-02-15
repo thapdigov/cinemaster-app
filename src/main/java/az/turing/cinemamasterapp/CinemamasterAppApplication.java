7519a0daf3505c597519232ebed02ddbce9e1371
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-02-15 23:26:24 +0400

    First Commit

---
+++ src/main/java/az/turing/cinemamasterapp/CinemamasterAppApplication.java
@@ -0,0 +1,13 @@
+package az.turing.cinemamasterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemamasterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemamasterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/turing/cinemamasterapp/CinemamasterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.turing.cinemamasterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemamasterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
