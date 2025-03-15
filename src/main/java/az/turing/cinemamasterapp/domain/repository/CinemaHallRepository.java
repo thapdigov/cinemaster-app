d9c61b9f3a7caf3c13ddad12eabb90896e606a42
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-03-15 22:10:57 +0400

    CinemaHall started to deployee

--- src/main/java/az/turing/cinemamasterapp/domain/repository/CinemaHallRepository.java
+++ src/main/java/az/turing/cinemamasterapp/domain/repository/CinemaHallRepository.java
@@ -3,5 +3,9 @@ package az.turing.cinemamasterapp.domain.repository;
 import az.turing.cinemamasterapp.domain.entity.CinemaHallEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CinemaHallRepository extends JpaRepository<CinemaHallEntity, Long> {
+
+    Optional<CinemaHallEntity> findByName(String name);
 }
